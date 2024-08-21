2db775741e186b217ed6fbe4f5b3edb1aa8f38fe
ItsTimmySV <josuegerardo00@gmail.com>
2024-08-20 23:31:21 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,50 @@
+const CACHE_NAME = 'v2';
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/javascripts.js',
+  '/img/icon-192x192.png',
+  '/img/icon-512x512.png',
+  '/manifest.json',
+  // Añadir más archivos si es necesario
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v2').then(cache => {
-      return cache.addAll([
-        '/index.html',
-        '/styles.css',
-        '/javascripts.js',
-        '/img/icon-192x192.png',
-        '/img/icon-512x512.png',
-        '/manifest.json',
-        // Otros archivos que quieras cachear
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => {
+          return caches.match('/index.html');
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
